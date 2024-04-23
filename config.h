6bc42ed0484d6a833b4ab32beaffb1638d3efe6b
s0m3sushi <ticothongminh@gmail.com>
2024-04-23 18:36:51 +0000

    oops i mean 17

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Cascadia Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Cascadia Mono:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
